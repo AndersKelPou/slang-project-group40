bdc0a133b7718de2b98dc28a04f152dcbd485d6e
Gideon <gidewadejob@gmail.com>
2024-10-12 15:12:04 +0100

    Added sequences and assignments WWWWWW Co-authored-by: Maxi35

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@ pub mod ivl;
 mod ivl_ext;
 
 use ivl::{IVLCmd, IVLCmdKind};
-use slang::ast::{Cmd, CmdKind, Expr};
+use slang::ast::{Cmd, CmdKind, Expr, Op, Ident};
 use slang_ui::prelude::*;
 
 pub struct App;
@@ -41,7 +41,10 @@ impl slang_ui::Hook for App {
             // from subsequent executions of the solver
             solver.scope(|solver| {
                 // Check validity of obligation
+                //println!("smt_obligation {:#?} ", soblig);
+                //println!("smt_obligation as bool {:#?} ", soblig.as_bool());
                 solver.assert(!soblig.as_bool()?)?;
+                //println!("we asserted right");
                 // Run SMT solver on all current assertions
                 match solver.check_sat()? {
                     // If the obligations result not valid, report the error (on
@@ -65,17 +68,26 @@ impl slang_ui::Hook for App {
 // Encoding of (assert-only) statements into IVL (for programs comprised of only
 // a single assertion)
 fn cmd_to_ivlcmd(cmd: &Cmd) -> Result<IVLCmd> {
+    //println!("cmd to ivlcmd {:#?}", &cmd.kind);
     match &cmd.kind {
         CmdKind::Assert { condition, .. } => Ok(IVLCmd::assert(condition, "Assert might fail!")),
-        _ => todo!("Not supported (yet)."),
+        CmdKind::Seq(cmd1, cmd2) => Ok(IVLCmd::seq(&cmd_to_ivlcmd(cmd1)?, &cmd_to_ivlcmd(cmd2)?)),
+        //CmdKind::Return { expr } => Ok(IVLCmd::)
+        CmdKind::Assignment { name, expr } => Ok(IVLCmd::assign(name, expr)),
+        _ => todo!(" Not supported (yet)."),
     }
 }
 
 // Weakest precondition of (assert-only) IVL programs comprised of a single
 // assertion
-fn wp(ivl: &IVLCmd, _: &Expr) -> Result<(Expr, String)> {
+fn wp(ivl: &IVLCmd, expr_in: &Expr) -> Result<(Expr, String)> {
+    //println!("Finding WP of {:#?} with expr: {:#?}", &ivl.kind, &expr_in.kind);
     match &ivl.kind {
-        IVLCmdKind::Assert { condition, message } => Ok((condition.clone(), message.clone())),
+        IVLCmdKind::Assert { condition, message } => Ok((Expr::op(&condition.clone(), Op::And, &expr_in), message.clone())),
+        IVLCmdKind::Seq(cmd1, cmd2) => {let (expr2, msg2) = wp(cmd2, expr_in)?;
+                                        let (expr1, msg1) = wp(cmd1, &expr2)?;
+                                        Ok((expr1,  msg1 ))}, // should not be just msg1
+        IVLCmdKind::Assignment { name, expr } => Ok((expr_in.clone().subst(|x| x.is_ident(&name.ident), expr), name.to_string())),//expr_in[var -> expr]
         _ => todo!("Not supported (yet)."),
     }
 }
