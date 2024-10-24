83599b41ce8c74b27ad1906bc093b6fcb64647e7
AndersKelPou <anders.poulsen.kp@gmail.com>
2024-10-24 16:33:53 +0200

    partial support for swp with the intention to localize errors

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ pub mod ivl;
 mod ivl_ext;
 
 use ivl::{IVLCmd, IVLCmdKind};
-use slang::ast::{Cmd, CmdKind, Expr, Op, ExprKind};
+use slang::ast::{Cmd, CmdKind, Expr, PrefixOp, ExprKind};
 use slang::Span;
 use slang_ui::prelude::*;
-
-pub struct App;
+use std::collections::HashSet;
+pub struct App; 
 
 impl slang_ui::Hook for App {
     fn analyze(&self, cx: &mut slang_ui::Context, file: &slang::SourceFile) -> Result<()> {
@@ -47,37 +47,57 @@ impl slang_ui::Hook for App {
             //println!("cmd {:#?}", &cmd);
             //println!("ivl {:#?}", &ivl);
             
-            let (oblig, msg) = wp(&ivl, &post)?;
+            let oblig = swp(&ivl, vec![(post, "Error in ensures ".to_string())])?;
             //println!("Span {:#?}", Span::default());
             //println!("oblig {:#?}", &oblig);
             // Convert obligation to SMT expression
-            let soblig = oblig.smt()?;
 
             // Run the following solver-related statements in a closed scope.
             // That is, after exiting the scope, all assertions are forgotten
             // from subsequent executions of the solver
-            solver.scope(|solver| {
-                // Check validity of obligation
-                //println!("smt_obligation {:#?} ", soblig);
-                //println!("smt_obligation as bool {:#?} ", soblig.as_bool());
-                solver.assert(!soblig.as_bool()?)?;
-                //println!("we asserted right");
-                // Run SMT solver on all current assertions
-                match solver.check_sat()? {
-                    // If the obligations result not valid, report the error (on
-                    // the span in which the error happens)
-                    smtlib::SatResult::Sat => {
-                        cx.error(oblig.span, format!("{msg}"));
+            
+            // Loop over expr in oblig 
+            //Tror vi skal loopp her
+            for i in 0..oblig.len() {
+                solver.scope(|solver| {
+                    let mut or_oblig = Expr::bool(false);
+                    let mut last_obl = Expr::bool(false); let mut last_msg = "".to_string();
+                    for (obl, msg) in oblig[0..i+1].iter() {
+                        or_oblig = or_oblig.or(&obl.prefix(PrefixOp::Not));
+                        last_obl = obl.clone();
+                        last_msg = msg.clone()
                     }
-                    smtlib::SatResult::Unknown => {
-                        cx.warning(oblig.span, "{msg}: unknown sat result");
+                    if let Ok(soblig) = or_oblig.smt() { 
+                        solver.assert(soblig.as_bool()?)?;
+                        //println!("we asserted right");
+                        // Run SMT solver on all csdurrent assertionsasd
+                        match solver.check_sat()? {
+                            // If the obligations result not validasd, report the error (on
+                            // the span in which the error happens)
+                            smtlib::SatResult::Sat => {
+                                cx.error(last_obl.span, format!("{last_msg}"));
+                            }
+                            smtlib::SatResult::Unknown => {
+                                cx.warning(last_obl.span, "{msg}: unknown sat result");
+                            }
+                            smtlib::SatResult::Unsat => (),
+                        }
+                    } else { // Er Ok() altid en success? eller kan det også være en error? forstår det ikke (seriøst), det kan ikke være en error hvis det er Ok()
+                        println!("{:#?}", or_oblig);
+                        todo!("Smt failed to generate(somehow???????)")
                     }
-                    smtlib::SatResult::Unsat => (),
-                }
-                Ok(())
-            })?;
+                    // Check validity of obligation
+                    //println!("smt_obligation {:#?} ", soblig);
+                    //println!("smt_obligation as bool {:#?} ", soblig.as_bool());
+    
+                    //First loop iter oblig[0]
+                    //Second loop iter oblig[0..1]
+                    //Third loop iter oblig[0..2]
+                    //...
+                    Ok(())
+                })?;
+            }
         }
-
         Ok(())
     }
 }
@@ -94,58 +114,75 @@ fn cmd_to_ivlcmd(cmd: &Cmd) -> Result<IVLCmd> {
                                                                     },
         CmdKind::Assignment     { name, expr }                  => Ok(IVLCmd::assign(name, expr)),
         CmdKind::Assume         { condition }                   => Ok(IVLCmd::assume(condition)),
-        CmdKind::Loop           { invariants, variant, body}    => Ok(IVLCmd::_loop(invariants, variant, body)),
-        CmdKind::Match          { body }                        => {    let mut cases = Vec::new();
-                                                                        for i in 0..body.cases.len() {
-                                                                            cases.push(cmd_to_ivlcmd(&Cmd::seq(&Cmd::assume(&body.cases[i].condition), &body.cases[i].cmd))?);
-                                                                        }
-                                                                        Ok(IVLCmd::nondets(&cases))
+        CmdKind::Loop           { invariants, variant, body}    => {Ok(IVLCmd::_loop(invariants, variant, body))},
+        CmdKind::Match          { body }                        => {let mut cases = Vec::new();
+                                                                    for i in 0..body.cases.len() {
+                                                                        cases.push(cmd_to_ivlcmd(&Cmd::seq((&Cmd::assume(&body.cases[i].condition)), &body.cases[i].cmd))?);
                                                                     }
+                                                                    Ok(IVLCmd::nondets(&cases))
+                                                                    }   
         CmdKind::Return         { expr }                        => Ok(IVLCmd::_return(expr)),
         _ => todo!(" Not supported (yet)."),
     }
 }
 
-// Weakest precondition of (assert-only) IVL programs comprised of a single
-// assertion
-fn wp(ivl: &IVLCmd, post: &Expr) -> Result<(Expr, String)> {
+// Set *based* weakest precondition
+fn swp<'a>(ivl: &IVLCmd, mut post: Vec<(Expr, String)>) -> Result<(Vec<(Expr, String)>)> {
     //println!("Finding WP of {:#?} with expr: {:#?}", &ivl.kind, &expr_in.kind);
     match &ivl.kind {
-        IVLCmdKind::Assert { condition, message }       => Ok((condition.clone().and(&post), message.clone())),
-        IVLCmdKind::Assume { condition }                => Ok((condition.clone().imp(&post), "Assume might fail".to_string())),
+        IVLCmdKind::Assert { condition, message }       => { post.push((condition.clone(), message.clone())); Ok(post) }
+        IVLCmdKind::Assume { condition }                => { let mut new_post = Vec::new();
+                                                             for (expr, msg) in post.iter() {
+                                                                new_post.push((condition.imp(expr), msg.clone()))
+                                                             }
+                                                             Ok(new_post)
+                                                            }
         IVLCmdKind::Seq(cmd1, cmd2)                     => { if !matches!(cmd1.kind, IVLCmdKind::Return{..}) {
-                                                                let (expr2, msg2) = wp(cmd2, post)?;
-                                                                let (expr1, msg1) = wp(cmd1, &expr2)?;
-                                                                Ok((expr1,  msg2))
+                                                                let post2 = swp(cmd2, post)?;   
+                                                                let post1 = swp(cmd1, post2)?;
+                                                                Ok(post1)
                                                             } else {
                                                                 let IVLCmdKind::Return{ ref expr } = cmd1.kind else { todo!("Won't ever happen") };
                                                                 if let Some(acc_expr) = expr {
                                                                     let mut temp_expr = Expr::bool(false);
                                                                     temp_expr.span = acc_expr.span;
                                                                     temp_expr.span = Span::from_start_end(temp_expr.span.start() - 7, temp_expr.span.end());
-                                                                    Ok((temp_expr,  "Jeg er dum og mit return er ikke til sidst".to_string()))
+                                                                    Ok(vec![(temp_expr, "Jeg er dum og mit return er ikke til sidst".to_string())])
                                                                 } else {
-                                                                    Ok((Expr::bool(false),  "Jeg er dum og mit return er ikke til sidst (no expr)".to_string()))
+                                                                    Ok(vec![(Expr::bool(false), "Jeg er dum og mit return er ikke til sidst (no expr)".to_string())])
                                                                 }
                                                             }
                                                             }
-        IVLCmdKind::Assignment { name, expr }           => Ok((post.clone().subst(|x| x.is_ident(&name.ident), expr), ["Failed in assigning: ", &name.to_string(), " with expression ", &expr.to_string()].join(""))),//post[var -> expr]
-        IVLCmdKind::Loop {invariants, variant, cases}   => { let mut precondition = Expr::bool(true);
-                                                            for invariant in invariants.iter() {
-                                                                precondition = invariant.clone().and(&precondition);
-                                                            };
-                                                            Ok((precondition.clone(), "Something wrong with the Loop yo".to_string()))// i && post
-                                                            },
-        IVLCmdKind::NonDet( cmd1, cmd2 )                => {let (expr0, msg0) = wp(cmd1, post)?;
-                                                            let (expr1, msg1) = wp(cmd2, post)?;
-                                                            Ok((expr0.clone().and(&expr1), msg0))
-                                                            },
-        IVLCmdKind::Return { expr }                     => {if let Some(expr) = expr {
-                                                                let _message = ["Failed in returning expression: ", (&expr.to_string())].join("");
-                                                                Ok((post.clone().subst(|x| matches!(x.kind, ExprKind::Result{..}), expr).clone(), _message.to_string()))
-                                                            } else {
-                                                                Ok((post.clone(), "Failed returning".to_string()))
+        IVLCmdKind::Assignment { name, expr }           => {let mut new_post = Vec::new();
+                                                            for (post_expr, msg) in post.iter() { 
+                                                                new_post.push((post_expr.clone().subst(|x| x.is_ident(&name.ident), expr), ["Failed in assigning: ", &name.to_string(), " with expression ", &expr.to_string()].join("")));
                                                             }
+                                                            Ok(new_post) //post[var -> expr]
+                                                            }
+        IVLCmdKind::Loop {invariants, variant, cases}   => {
+            
+                                                            // https://pv24.cmath.eu/06-loops.html#weakest-preconditions
+                                                             //let case = cases.cases[0];
+                                                             //b -> wp[C](wp[while (b){C}](G)) /\ !b -> G
+                                                             //let res = case.condition.imp(swp(case.cmd, swp(&ivl,post))).and(!case.condition.and(post))
+                                                             //Ok(vec![(res.clone(), "Something wrong with the Loop yo".to_string())])
+
+                                                             let precondition = Expr::bool(true);
+                                                             Ok(vec![(precondition.clone(), "Something wrong with the Loop yo".to_string())])// i && post
+                                                            },
+        IVLCmdKind::NonDet( cmd1, cmd2 )                => {let mut post1 = swp(cmd1, post.clone())?;
+                                                            let post2 = swp(cmd2, post.clone())?;
+                                                            post1.extend(post2);
+                                                            Ok(post1)
+                                                            },
+        IVLCmdKind::Return { expr }                     => {if let Some(acc_expr) = expr {
+                                                                let mut new_post = Vec::new();
+                                                                for (post_expr, msg) in post.iter() { 
+                                                                    let _message = ["Failed in returning expression: ", (&acc_expr.to_string())].join("");
+                                                                    new_post.push((acc_expr.clone().subst(|x| matches!(x.kind, ExprKind::Result{..}), post_expr).clone(), _message.to_string()));
+                                                                }
+                                                                Ok(new_post)
+                                                            } else { Ok(post) }
                                                             },
         _ => todo!("Not supported (yet)."),
     }
